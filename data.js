d1359a7cc61f9e55687d108cd794eb5966340be7
8ofAce <frograinbow03@gmail.com>
2025-12-15 17:44:55 -0500

    Update thumbnail URL for Escape Car game

--- data.js
+++ data.js
@@ -9,7 +9,7 @@ const games = [
     title: "Escape Car",
     description: "Do you remember the very first Grand Theft Auto? That old-school, top-down chaos. Think of that and give it a fresh coat of chunky 3D graphics. Next, drop that straight in your browser. Yes, it's Escape Car, a free online GTA game where you bolt through a mini-city, dodge police cars, tanks, helicopters and scoop up stacks of cash. Every tight drift, every split-second fake-out, and every totaled police cruiser bumps your score higher. Hang on long enough, and you’ll leave with pockets full of upgrade money and a grin wider than the avenue.",
     embedUrl: "https://cloud.onlinegames.io/games/2025/unity2/escape-car/index-og.html",
-    thumbnail: "https://www.onlinegames.io/escape-car/" 
+    thumbnail: "https://i.postimg.cc/gjF1FdMj/IMG-5251.jpg" 
   },
   {
     title: "Drift King",
